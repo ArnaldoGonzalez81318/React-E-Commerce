194ba17656f6ba669b5f7d80f164d99f2f7e62ff
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-07 13:27:47 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/navBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/shop';
+import ShopCategory from './Pages/shopCategory';
+import Product from './Pages/product';
+import LoginSignup from './Pages/loginSignup';
+import Cart from './Pages/cart';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="mens" element={<ShopCategory category="mens" />} />
+          <Route path="womens" element={<ShopCategory category="womens" />} />
+          <Route path="kids" element={<ShopCategory category="kids" />} />
+          <Route path="product/" element={<Product />}>
+            <Route path=":id" element={<Product />} />
+          </Route>
+          <Route path="login" element={<LoginSignup />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
