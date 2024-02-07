7bcff891cb700f62371c8d3986b9a454273fb27c
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-07 13:27:57 -0500

    Update navBar.jsx

--- frontend/src/Components/NavBar/navBar.jsx
+++ frontend/src/Components/NavBar/navBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import './navBar.css'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   const [menu, setMenu] = useState("Shop All")
@@ -12,19 +13,31 @@ export const NavBar = () => {
         <h1>Shop</h1>
       </div>
       <ul className='navbar-menu'>
-        <li onClick={() => setMenu("Shop All")} className={menu === "Shop All" ? "active" : ""}>Shop All</li>
-        <li onClick={() => setMenu("Mens")} className={menu === "Mens" ? "active" : ""}>Mens</li>
-        <li onClick={() => setMenu("Womens")} className={menu === "Womens" ? "active" : ""}>Womens</li>
-        <li onClick={() => setMenu("Kids")} className={menu === "Kids" ? "active" : ""}>Kids</li>
+        <li onClick={() => setMenu("Shop All")} className={menu === "Shop All" ? "active" : ""}>
+          <Link to="/">Shop All</Link>
+        </li>
+        <li onClick={() => setMenu("Mens")} className={menu === "Mens" ? "active" : ""}>
+          <Link to="mens">Mens</Link>
+        </li>
+        <li onClick={() => setMenu("Womens")} className={menu === "Womens" ? "active" : ""}>
+          <Link to="womens">Womens</Link>
+        </li>
+        <li onClick={() => setMenu("Kids")} className={menu === "Kids" ? "active" : ""}>
+          <Link to="kids">Kids</Link>
+        </li>
       </ul>
       <div className="navbar-login-cart">
-        <button
-          className="navbar-login-btn"
-          onClick={() => alert('Login')}>
-          Login
-        </button>
-        <img src={cart_icon} alt="cart" />
-        <span className="cart-count">0</span>
+        <Link to="login" >
+          <button
+            className="navbar-login-btn"
+            onClick={() => alert('Login')}>
+            Login
+          </button>
+        </Link>
+        <Link to="cart">
+          <img src={cart_icon} alt="cart" />
+          <span className="cart-count">0</span>
+        </Link>
       </div>
     </div>
   )
