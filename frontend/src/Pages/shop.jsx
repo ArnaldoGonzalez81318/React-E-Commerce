efa76540b6a8b84dcd71ac085758599684b95f82
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-07 13:28:47 -0500

    Create shop.jsx

---
+++ frontend/src/Pages/shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Components/Home/home'
+
+const Shop = () => {
+  return (
+    <div className="shop">
+      <Hero />
+    </div>
+  )
+}
+
+export default Shop
