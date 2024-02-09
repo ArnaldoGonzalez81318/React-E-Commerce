d41b9aebc174db773a0d1d7809b4fcc423953f35
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-08 21:27:41 -0500

    Update shop.jsx

--- frontend/src/Pages/shop.jsx
+++ frontend/src/Pages/shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Home/home'
 import Trending from '../Components/Trending/trending'
 import Offers from '../Components/Offers/offers'
+import NewCollections from '../Components/NewCollections/newCollections'
 
 const Shop = () => {
   return (
@@ -9,6 +10,7 @@ const Shop = () => {
       <Hero />
       <Trending />
       <Offers />
+      <NewCollections />
     </div>
   )
 }
