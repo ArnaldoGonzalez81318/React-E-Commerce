97c65bc2e4f76bcc7cc28493ebdc6caae5d17456
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-21 10:34:42 -0500

    Create descriptionBox.jsx

---
+++ frontend/src/Components/DescriptionBox/descriptionBox.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import './descriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className="description-box">
+      <div className="description-box-container">
+        <div className="description-box-navigator-container">
+          <div className="description-box-navigator-item active">Description</div>
+          <div className="description-box-navigator-item faded">Reviews (1,245)</div>
+        </div>
+        <div className="description-box-description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac dui et eros
+            posuere pharetra. Donec sed ligula nec nunc fermentum tincidunt. Nullam
+            tincidunt, arcu nec aliquam tincidunt, justo nunc fermentum odio, a
+            condimentum purus ex in quam. Donec id semper turpis. Sed vel odio
+            condimentum, fermentum nunc nec, varius turpis. Donec id semper turpis. Sed
+            vel odio condimentum, fermentum nunc nec, varius turpis.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac dui et eros
+            posuere pharetra. Donec sed ligula nec nunc fermentum tincidunt. Nullam
+            tincidunt, arcu nec aliquam tincidunt, justo nunc fermentum odio, a
+            condimentum purus ex in quam. Donec id semper turpis. Sed vel odio
+            condimentum, fermentum nunc nec, varius turpis. Donec id semper turpis. Sed
+            vel odio condimentum, fermentum nunc nec, varius turpis.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac dui et eros
+            posuere pharetra. Donec sed ligula nec nunc fermentum tincidunt. Nullam
+            tincidunt, arcu nec aliquam tincidunt, justo nunc fermentum odio, a
+            condimentum purus ex in quam. Donec id semper turpis. Sed vel odio
+            condimentum, fermentum nunc nec, varius turpis. Donec id semper turpis. Sed
+            vel odio condimentum, fermentum nunc nec, varius turpis.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
