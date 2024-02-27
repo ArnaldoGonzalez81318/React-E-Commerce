dc8fa48858cae0e8c8a11d9860aa7bdef7e741fd
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-02-26 21:59:13 -0500

    Create relatedProducts.jsx

---
+++ frontend/src/Components/RelatedProducts/relatedProducts.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import data_product from '../Assets/data'
+import Item from '../Item/item'
+
+import './relatedProducts.css'
+
+const RelatedProducts = () => {
+  return (
+    <div className="related-products">
+      <h2>Related Products</h2>
+      <hr />
+      <div className="related-products-container">
+        {data_product.map((product) => {
+          return (
+            <Item
+              key={product.id}
+              image={product.image}
+              name={product.name}
+              old_price={product.old_price}
+              new_price={product.new_price}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
