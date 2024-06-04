0270c84d3b7201fa38e4e197bff9ae03485d93ec
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2024-06-04 14:59:52 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,47 @@ const exp = require('constants');
 
 app.use(express.json());
 app.use(cors());
+
+// Connect to MongoDB database using mongoose package.
+mongoose.connect('mongodb+srv://arnaldolgonzalez96:jNfxhOUIu6tR0815@cluster0.ltsktky.mongodb.net/e-commerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// API routes.
+app.get('/', (req, res) => { // Home route.
+  res.send('Hello World!');
+});
+
+// Image upload route using multer package.
+const storage = multer.diskStorage({
+  destination: './uploads/images',
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+  }
+});
+
+// Upload image middleware.
+const upload = multer({
+  storage: storage
+});
+
+// Upload image endpoint.
+app.use('/images', express.static(path.join(__dirname, 'uploads/images'))); // Serve images from the uploads folder.
+
+// Upload image route. The image will be uploaded to the uploads folder and the image URL will be sent back to the client.
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json({ // Send the image URL back to the client.
+    success: 1,
+    profile_url: `http://localhost:${port}/images/${req.file.filename}`
+  });
+});
+
+// Start the server.
+app.listen(port, (err) => {
+  if (err) {
+    console.log('Error: ', err);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
+});
