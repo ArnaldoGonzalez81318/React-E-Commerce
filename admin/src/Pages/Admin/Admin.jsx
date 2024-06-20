6d5f5c7e9f315d044bed0c9f348bf3f5f43171f7
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-06-20 14:10:56 -0400

    Update Admin.jsx

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,20 @@
 // import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import AddProduct from '../../Components/AddProduct/AddProduct'
+import ProductList from '../../Components/ProductList/ProductList'
+import { Routes, Route } from 'react-router-dom'
 import './Admin.css'
 
 const Admin = () => {
   return (
     <div className="admin">
       <Sidebar />
+      <div className="admin-content">
+        <Routes>
+          <Route path="/add-product" element={<AddProduct />} />
+          <Route path="/product-list" element={<ProductList />} />
+        </Routes>
+      </div>
     </div>
   )
 }
