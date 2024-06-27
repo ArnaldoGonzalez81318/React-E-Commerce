99447ae567ee65cace61506ff12b832186236655
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2024-06-27 15:42:23 -0400

    Update shopContext.jsx

--- frontend/src/Context/shopContext.jsx
+++ frontend/src/Context/shopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext(null);
 const getDefaultCart = () => {
   let cart = {};
 
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 300 + 1; i++) {
     cart[i] = 0;
   }
 
