070a8ca65fa356e6a6b37b87893c81307f3ebdf6
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2024-06-27 15:42:21 -0400

    Update trending.jsx

--- frontend/src/Components/Trending/trending.jsx
+++ frontend/src/Components/Trending/trending.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import data_product from '../Assets/data'
+import React, { useState, useEffect } from 'react'
 import Item from '../Item/item'
 
 import './trending.css'
 
 const Trending = () => {
+  const [trendingProducts, setTrendingProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/trending-women')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Fetched data:', data);
+        setTrendingProducts(Array.from(data.trendingWomen));
+      })
+      .catch(err => {
+        console.log('Error:', err);
+      });
+  }, []);
+
   return (
     <div className="trending">
       <h2>Trending Women's Collection</h2>
       <hr />
       <div className="trending-items">
-        {data_product.map((item) => (
+        {trendingProducts.map((item) => (
           <Item key={item.id} image={item.image} name={item.name} old_price={item.old_price} new_price={item.new_price} />
         ))}
       </div>
