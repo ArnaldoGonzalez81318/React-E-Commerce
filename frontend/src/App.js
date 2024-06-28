247e3b24c693e45f3751393803f1d8228122e79d
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2024-06-28 00:25:00 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" banner={mens_banner} />} />
-          <Route path="/womens" element={<ShopCategory category="women" banner={women_banner} />} />
+          <Route path="/men" element={<ShopCategory category="men" banner={mens_banner} />} />
+          <Route path="/women" element={<ShopCategory category="women" banner={women_banner} />} />
           <Route path="/kids" element={<ShopCategory category="kid" banner={kids_banner} />} />
           <Route path="/product" element={<Product />}>
             <Route path=":id" element={<Product />} />
