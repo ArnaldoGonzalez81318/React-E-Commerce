95c2b91f333d2f0a459bb24e2cec9ac5afe8d44f
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-07-03 13:02:30 -0400

    Update breadcrumbs.jsx

--- frontend/src/Components/Breadcrumbs/breadcrumbs.jsx
+++ frontend/src/Components/Breadcrumbs/breadcrumbs.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import arrow_icon from '../Assets/breadcrumb_arrow.png'
 
 import './breadcrumbs.css'
 
@@ -12,11 +11,8 @@ const Breadcrumbs = (props) => {
     <div className='breadcrumbs'>
       <div className="breadcrumbs-container">
         <Link to='/'>Home</Link>
-        {/* <img src={arrow_icon} alt="arrow" /> */}
         <Link to='/shop'>Shop</Link>
-        {/* <img src={arrow_icon} alt="arrow" /> */}
         <Link to={`/category/${product.category}`}>{product.category}</Link>
-        {/* <img src={arrow_icon} alt="arrow" /> */}
         <p>{product.name}</p>
       </div>
     </div>
