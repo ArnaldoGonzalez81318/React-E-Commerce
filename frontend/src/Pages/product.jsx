b433b8dce211a20165ba2e83b9ca3b9c5454c122
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-07-03 13:51:07 -0400

    Update product.jsx

--- frontend/src/Pages/product.jsx
+++ frontend/src/Pages/product.jsx
@@ -4,14 +4,15 @@ import { useParams } from 'react-router-dom';
 import Breadcrumbs from '../Components/Breadcrumbs/breadcrumbs';
 import ProductDisplay from '../Components/ProductDisplay/productDisplay';
 import DescriptionBox from '../Components/DescriptionBox/descriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/relatedProducts';
 
 import './CSS/product.css';
-import RelatedProducts from '../Components/RelatedProducts/relatedProducts';
 
 const Product = () => {
   const { all_products } = useContext(ShopContext);
   const { id } = useParams();
   const product = all_products.find((e) => e.id === Number(id));
+  // const product = all_products.find((product) => product.id === Number(id));
 
   return (
     <div className='product'>
