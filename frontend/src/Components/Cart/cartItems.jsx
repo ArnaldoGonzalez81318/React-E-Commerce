3756ec39d046a25d315a15af62d570c5882f319e
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2024-07-12 09:52:16 -0400

    Update cartItems.jsx

--- frontend/src/Components/Cart/cartItems.jsx
+++ frontend/src/Components/Cart/cartItems.jsx
@@ -5,7 +5,7 @@ import './cartItems.css';
 
 const CartItems = () => {
   const {
-    all_products,
+    allProducts,
     cartItems,
     addToCart,
     removeFromCart,
@@ -24,32 +24,35 @@ const CartItems = () => {
           <h3>Total</h3>
           <h3>Remove</h3>
         </div>
-
         <div className='cart-items-list'>
-          {all_products.map((product) => {
-            if (cartItems[product.id] > 0) {
-              return (
-                <div className='cart-item-wrapper' key={product.id}>
-                  <div className='cart-item'>
-                    <img src={product.image} alt={product.name} className='cart-item-image' />
-                    <p className='cart-item-title'>{product.name}</p>
-                    <div className='quantity'>
-                      <button onClick={() => removeFromCart(product.id)}>-</button>
-                      <p>{cartItems[product.id]}</p>
-                      <button onClick={() => addToCart(product.id)}>+</button>
-                    </div>
-                    <p>${product.new_price}</p>
-                    <p>${(cartItems[product.id] * product.new_price).toFixed(2)}</p>
-                    <div className='remove-item' onClick={() => removeFromCart(product.id)}>
-                      <img src={remove_icon} className='remove-icon' alt='remove' />
+          {allProducts && allProducts.length > 0 ? (
+            allProducts.map((product) => {
+              if (cartItems[product.id] > 0) {
+                return (
+                  <div className='cart-item-wrapper' key={product.id}>
+                    <div className='cart-item'>
+                      <img src={product.image} alt={product.name} className='cart-item-image' />
+                      <p className='cart-item-title'>{product.name}</p>
+                      <div className='quantity'>
+                        <button onClick={() => removeFromCart(product.id)}>-</button>
+                        <p>{cartItems[product.id]}</p>
+                        <button onClick={() => addToCart(product.id)}>+</button>
+                      </div>
+                      <p>${product.new_price}</p>
+                      <p>${(cartItems[product.id] * product.new_price).toFixed(2)}</p>
+                      <div className='remove-item' onClick={() => removeFromCart(product.id)}>
+                        <img src={remove_icon} className='remove-icon' alt='remove' />
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
+                );
+              } else {
+                return null;
+              }
+            })
+          ) : (
+            <p>No items in cart</p>
+          )}
         </div>
       </div>
 
