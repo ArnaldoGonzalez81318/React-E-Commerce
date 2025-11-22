3c4aad994d79f797768db3ab18b23eaa4d8e6228
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2025-11-22 02:49:30 -0500

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Admin from "./Pages/Admin/Admin";
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import AdminLayout from './Pages/Admin/Admin'
+import AddProduct from './Components/AddProduct/AddProduct'
+import ProductList from './Components/ProductList/ProductList'
+import EditProduct from './Components/EditProduct/EditProduct'
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-slate-50">
       <Navbar />
       <Routes>
-        <Route path="/*" element={<Admin />} />
+        <Route path="/" element={<AdminLayout />}>
+          <Route index element={<Navigate to="add-product" replace />} />
+          <Route path="add-product" element={<AddProduct />} />
+          <Route path="product-list" element={<ProductList />} />
+          <Route path="edit-product/:productId" element={<EditProduct />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/add-product" replace />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
